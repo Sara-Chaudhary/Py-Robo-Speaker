bbb99dc87d399d89ec44d280a14cc9f56cd0897c
Sara-Chaudhary <sara.siwaya@gmail.com>
2023-09-16 21:59:01 +0530

    Adding first version of python project

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+if __name__ == '__main__':
+    print("Welcome to Robospeaker 1.1 ")
+    while (True):
+        x = input("Enter what do you want me to pronounce : ")
+        if x == "q":
+            os.system("say 'bye bye friend'") 
+            break
+        command = f"say {x}"
+        os.system(command)
