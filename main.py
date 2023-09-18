a8cfea141276365ce0b43dd0efeb28eeab1aeb91
Sara-Chaudhary <sara.siwaya@gmail.com>
2023-09-18 15:54:34 +0530

    changing the terminating condition

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     print("Welcome to Robospeaker 1.1 ")
     while (True):
         x = input("Enter what do you want me to pronounce : ")
-        if x == "q":
+        if x == "bye":
             os.system("say 'bye bye friend'") 
             break
         command = f"say {x}"
